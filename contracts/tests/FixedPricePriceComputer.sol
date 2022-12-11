d8f6433e3d6607e2f4950cc05778a17da47dfdd2
Illya Gerasymchuk <illya@iluxonchik.me>
2022-12-11 20:08:08 +0200

    test: add price dynamic price computer that returns a static price

---
+++ contracts/tests/FixedPricePriceComputer.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.0;
+import "interfaces/IPriceComputer.sol";
+
+contract FixedPricePriceComputer is IPriceComputer {
+    uint256 public price;
+
+    constructor(uint256 _price) {
+        price = _price;
+    }
+
+    function getPriceForToken(uint256 paymentRequestId, address tokenAddr, address payer) external returns (uint256) {
+        return price;
+    }
+}
