89f09e4dcfc2ae1868e862682091bcd2e23e7ca5
Illya Gerasymchuk <illya@iluxonchik.me>
2022-12-25 22:48:40 +0200

    test: update naming in test contracts

---
+++ contracts/tests/OnePurchasePerAddressPaymentPrecondition.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "interfaces/IPaymentPrecondition.sol";
+import "contracts/PaymentRequest.sol";
+
+/// @notice Sample payment precondition contract that allows payment in a particular token only if the address has not purchased
+/// the paymentRequestId in question.
+contract NFTOwnerPaymentPrecondition is IPaymentPrecondition {
+
+    function isPaymentAllowed(uint256 paymentRequestId, address payer, address token) external override returns(bool) {
+        PaymentRequest paymentRequest = PaymentRequest(msg.sender);
+        Receipt receipt = paymentRequest.receipt();
+        
+    }
+}
