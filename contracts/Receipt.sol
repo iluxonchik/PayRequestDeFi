be6ba5f1315ff2cf621079824402e3c33073639a
Illya Gerasymchuk <illya@iluxonchik.me>
2022-12-25 22:49:15 +0200

    feat: add helper functions for addition and removal of Receipts per paymentRequestId

--- contracts/Receipt.sol
+++ contracts/Receipt.sol
@@ -15,7 +15,15 @@ contract Receipt is ERC721Enumerable, Ownable {
     Counters.Counter internal _tokenId;
 
     mapping(uint256 => Payment.ReceiptData) internal receipt;
+    // paymentRequestId --> address (Payer) --> receiptIds paid by Payer
     mapping(address => uint256[]) internal receiptIdsPaidByAddr;
+    mapping(uint256 => mapping(address => uint256[])) internal receiptIdsPaidByAddrForPaymentRequestId;
+    
+    mapping(uint256 => mapping(address => uint256)) internal _balancesForPaymentRequestId;
+    mapping(uint256 => mapping(address => mapping(uint256 => uint256))) internal _ownedTokensForPaymentRequestId;
+    mapping(uint256 => mapping(uint256 => uint256)) internal _ownedTokensIndexForPaymentRequestId;
+    mapping(uint256 => uint256[]) internal _allTokensForPaymentRequestId;
+    mapping(uint256 => mapping(uint256 => uint256)) _allTokensIndexForPaymentRequestId;
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
 
@@ -24,9 +32,6 @@ contract Receipt is ERC721Enumerable, Ownable {
         _mint(payer, receiptId);
         _tokenId.increment();
 
-        // technically, this receipt can be emitted by anyone, so msg.sender can be arbitrary. as such, you need to verify
-        // what paymentRequest points to. for a Receipt that is only emittable by a particular PaymentRequest contract
-        // see ExclusiveReceipt.sol
         receipt[receiptId] = Payment.ReceiptData(
             {
                 paymentRequest: msg.sender, // PaymentRequest that emitted the receipt
@@ -38,9 +43,86 @@ contract Receipt is ERC721Enumerable, Ownable {
             }
         );
         receiptIdsPaidByAddr[payer].push(receiptId);
+        receiptIdsPaidByAddrForPaymentRequestId[paymentRequestId][payer].push(receiptId);
         
         return receiptId;
 
+    }
+
+    function balanceOfForPaymentRequestId(uint256 paymentRequestId, address owner) public view virtual returns (uint256) {
+        return _balancesForPaymentRequestId[paymentRequestId][owner];
+    }
+
+    /*
+        // https://docs.openzeppelin.com/contracts/4.x/api/token/erc721#ERC721-_beforeTokenTransfer-address-address-uint256-uint256-
+        // "from" and "to" non-zero: "from"'s token is transferred to "to".
+        // "from" is zero: token minted to "to".
+        // "to" is zero: "from"'s token will be burned. Since ERC721Burnable is not implemented, and _burn() is not called within this
+        //     smart contract's code, it will never be 0
+        // since ERC721Consecutive is not implemented, batchSize will always be 1
+    */
+    function _beforeTokenTransfer(address from, address to, uint256 receiptId, uint256 batchSize) internal virtual override {
+        super._beforeTokenTransfer(from, to, receiptId, batchSize);
+        
+        require(to != address(0), "Receipt: Burining of tokens is not permitted");
+        
+        Payment.ReceiptData memory receiptData = receipt[receiptId];
+        uint256 paymentRequestId = receiptData.paymentRequestId;
+
+        // First, deal with balances changes
+        if (from != address(0)) {
+            // receipt is being transferred from "from" to "to" (i.e. non-mint operation)
+            _balancesForPaymentRequestId[paymentRequestId][from] -= 1;
+        }
+        // "to" is never 0, so its balance will always be incremented
+        _balancesForPaymentRequestId[paymentRequestId][to] += 1;
+
+
+        // TODO: complete below
+        // Now, take care of altering collections as needed
+        if (from == address(0)) {
+            _addTokenToAllTokensEnumerationForPaymentRequestId()
+        }
+        
+    }
+
+    function _addTokenToAllTokensEnumerationForPaymentRequestId(uint256 paymentRequestId, uint256 tokenId) private {
+        _allTokensIndexForPaymentRequestId[paymentRequestId][tokenId] = _allTokensForPaymentRequestId[paymentRequestId].length;
+        _allTokensForPaymentRequestId[paymentRequestId].push(tokenId);
+    }
+
+    function _addTokenToOwnerEnumerationForPaymentRequestId(uint256 paymentRequestId, address to, uint256 receiptId) private {
+        uint256 length = balanceOfForPaymentRequestId(paymentRequestId, to);
+        _ownedTokensForPaymentRequestId[paymentRequestId][to][length] = receiptId;
+        _ownedTokensIndexForPaymentRequestId[paymentRequestId][receiptId] = length;
+    }
+
+    function _removeTokenFromAllTokensEnumerationForPaymentRequestId(uint256 paymentRequestId, uint256 receiptId) private {
+        // Move last receipt into the place of the token to remove, and delete the last index
+        uint256 lastReceiptIndex = _allTokensForPaymentRequestId[paymentRequestId].length;
+        uint256 lastReceiptId = _allTokensForPaymentRequestId[lastReceiptIndex];
+        uint256 indexOfReceiptToRemove = _allTokensIndexForPaymentRequestId[paymentRequestId][receiptId];
+
+        _allTokensForPaymentRequestId[paymentRequestId[indexOfReceiptToRemove] = lastReceiptId;
+        _allTokensIndexForPaymentRequestId[paymentRequestId][lastReceiptId] = indexOfReceiptToRemove;
+
+        delete _allTokensIndexForPaymentRequestId[paymentRequestId][receiptId];
+        _allTokensForPaymentRequestId[paymentRequestId].pop();
+    }
+
+    function _removeTokenFromOwnerEnumerationForPaymentRequestId(uint256 paymentRequestId, address from, uint256 receiptId) private {
+        uint256 lastReceiptIndex = balanceOfForPaymentRequestId(paymentRequestId, from) - 1;
+        uint256 receiptToRemoveIndex = _ownedTokensIndexForPaymentRequestId[paymentRequestId][receiptId];
+
+        if (lastReceiptIndex != receiptToRemoveIndex) {
+            // move the last receipt into the position of the receipt to remove 
+            uint256 lastReceipt = _ownedTokensForPaymentRequestId[paymentRequestId][from][lastReceiptIndex];
+            _ownedTokensForPaymentRequestId[paymentRequestId][from][receiptToRemoveIndex] = lastReceipt;
+            _ownedTokensIndexForPaymentRequestId[paymentRequestId][lastReceipt] = receiptToRemoveIndex;
+        }
+
+        delete _ownedTokensForPaymentRequestId[paymentRequestId][from][lastReceiptIndex];
+        delete _ownedTokensIndexForPaymentRequestId[paymentRequestId][receiptId];
     }
 
     function getReceiptData(uint256 receiptId) public view returns (Payment.ReceiptData memory) {
