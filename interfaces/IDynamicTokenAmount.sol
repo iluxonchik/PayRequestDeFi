2651ab9b58178fddbf837e1e0b923918fa51626a
Illya Gerasymchuk <illya@iluxonchik.me>
2023-01-18 22:50:33 +0000

    feat: update interfaces for PayFor functionality
    Add "beneficiary" parameter

--- interfaces/IDynamicTokenAmount.sol
+++ interfaces/IDynamicTokenAmount.sol
@@ -7,10 +7,10 @@ interface IDynamicTokenAmount {
     // it can be implemented as a part of a separate interface.
 
     /// @notice Returns the amount of tokens for a particular paymentRequest ID.
-    function getAmountForToken(uint256 paymentRequestId, address token, address payer) external returns (uint256);
+    function getAmountForToken(uint256 paymentRequestId, address token, address payer, address beneficiary) external returns (uint256);
 
     // TODO: add isTokenAccepted() method? this would allow the swapper to query for dynamic tokens.
-    // do we really want to add this to the PaymentRequest? this would enforce all implemenetations to
+    // do we really want to add this to the PaymentRequest? this would enforce all implementations to
     // create it. could it be bad to enforce this on all? what about generic payments?
-    function isTokenAccepted(uint256 paymentRequestId, address token, address payer) external returns (bool);
+    function isTokenAccepted(uint256 paymentRequestId, address token, address payer, address beneficiary) external returns (bool);
  }
